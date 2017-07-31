a148013c5a60b86d9165886f8b4721e3404535b5
Baroti Csaba <baroti.csaba.ubb@gmail.com>
2017-08-01 01:26:19 +0300

    Separeted from server

--- subtitle-clustering/start-server.js
+++
@@ -1,7 +0,0 @@
-const args = ['start','D:\\VM\\subtitles_source\\opensubtitles\\test','D:\\VM\\pdae-repo-python'];
-const opts = {
-    stdio: 'inherit',
-    cwd: 'server',
-    shell: true
-};
-require('child_process').spawn('npm', args, opts);
